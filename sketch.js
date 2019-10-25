809f461897199860b9bda4e4767743261af756b7
kctang97 <42600194+kctang97@users.noreply.github.com>
2019-10-24 17:16:06 -0700

    project
    s gh

---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+let xpos, ypos; // Starting position of shape
+let rad = 80; // Width of the shape
+let xspeed = 3; // Speed of the shape
+let yspeed = 2.5; // Speed of the shape
+let xdirection = 1; // Left or Right
+let ydirection = 1; // Top to Bottom
+
+let img;
+let s;
+
+function preload() {
+  img = loadImage("image/ghost.png");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  frameRate(20);
+  // ellipseMode(RADIUS);
+  // Set the starting position of the shape
+  xpos = windowWidth / 1.5;
+  ypos = windowHeight / 1.5;
+
+  s = createAudio("sound/heartbeat1.mp3");
+
+}
+
+function draw() {
+    background(0, 60);
+    ball();
+  // Update the position of the shape
+
+}
+
+function ball() {
+  xpos = xpos + xspeed * xdirection;
+  ypos = ypos + yspeed * ydirection;
+
+  // Test to see if the shape exceeds the boundaries of the screen
+  // If it does, reverse its direction by multiplying by -1
+  if (xpos > width - rad || xpos < rad) {
+    xdirection *= -1;
+  }
+  if (ypos > height - rad || ypos < rad) {
+    ydirection *= -1;
+  }
+  // Draw the shape
+  image(img, xpos, ypos, rad, rad);
+  tint(200, random(100));
+}
+
+function mousePressed() {
+  if(dist(mouseX, mouseY, xpos, ypos) < 80) {
+
+    s.play(true);
+    // s.volume(100);
+    // console.log("click");
+  }
+}
